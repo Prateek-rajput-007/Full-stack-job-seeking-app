a21098bf47f448f4658318b24fd801a8810a80a0
Prateek <109753518+Prateek-rajput-007@users.noreply.github.com>
2025-02-16 21:27:21 +0530

    Update PostJob.jsx

--- frontend/src/components/Job/PostJob.jsx
+++ frontend/src/components/Job/PostJob.jsx
@@ -34,7 +34,7 @@ const PostJob = () => {
   
     await axios
       .post(
-        "http://localhost:4000/api/v1/job/post",
+        "https://js-seeker.onrender.com/api/v1/job/post",
         fixedSalary.length >= 4
           ? {
               title,
