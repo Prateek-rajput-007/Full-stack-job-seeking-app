35f105a7310f12d21fc517e07ef8b15e3b1f7cd5
Prateek <109753518+Prateek-rajput-007@users.noreply.github.com>
2025-02-16 21:26:48 +0530

    Update Jobs.jsx

--- frontend/src/components/Job/Jobs.jsx
+++ frontend/src/components/Job/Jobs.jsx
@@ -12,7 +12,7 @@ const Jobs = () => {
   useEffect(() => {
     try {
       axios
-        .get("http://localhost:4000/api/v1/job/getall", {
+        .get("https://js-seeker.onrender.com/api/v1/job/getall", {
           withCredentials: true,
         })
         .then((res) => {
